601546c9f0d905b0d41bcc9c8f77302da033fe72
NGUYEN TUAN NGOC <45462425+tuanngoc101084@users.noreply.github.com>
2019-11-07 06:28:30 +0900

    Add button and constraint

--- app/src/main/java/com/example/freakingmath08102019/MainActivity.java
+++ app/src/main/java/com/example/freakingmath08102019/MainActivity.java
@@ -13,8 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Random random= new Random();
-            int number = random.nextInt(6)+5;
-            Toast.makeText(this, String.valueOf(number), Toast.LENGTH_SHORT).show();
+
     }
 }
